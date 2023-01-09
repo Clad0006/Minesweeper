9d5f9e9beb130ec95cddac98aa67a4e9790b0084
VALENTIN CLADEL <valentin.cladel@etudiant.univ-reims.fr>
2023-01-09 17:05:22 +0100

    Ajout de fin de scéance

--- pydemineur/Model/GrilleDemineur.py
+++ pydemineur/Model/GrilleDemineur.py
@@ -30,7 +30,7 @@ def type_grille_demineur(grille: list) -> bool:
     if nc == 0:
         return False
     return next(filterfalse(lambda line: type(line) == list and len(line) == nc
-                            and next(filterfalse(type_cellule, line), True) is True, grille), True) is True
+                                         and next(filterfalse(type_cellule, line), True) is True, grille), True) is True
     # Tableau régulier
     # nc = None
     # for line in grille:
@@ -51,68 +51,101 @@ def type_grille_demineur(grille: list) -> bool:
     #         return False
     # return True
 
-def construireGrilleDemineur(l:int,c:int)->list:
-    grille=[]
-    if l<=0 or c<=0:
-        raise ValueError("construireGrilleDemineur : Le nombre de lignes",l," ou de colonnes",c," est négatif ou nul.")
+
+def construireGrilleDemineur(l: int, c: int) -> list:
+    grille = []
+    if l <= 0 or c <= 0:
+        raise ValueError("construireGrilleDemineur : Le nombre de lignes", l, " ou de colonnes", c,
+                         " est négatif ou nul.")
     else:
         for i in range(l):
-            ligne=[]
+            ligne = []
             for i in range(c):
-                cell=construireCellule(0,False)
+                cell = construireCellule(0, False)
                 ligne.append(cell)
             grille.append(ligne)
     return grille
 
-def getNbLignesGrilleDemineur(grille:list)->int:
-    if type_grille_demineur(grille)==False:
+
+def getNbLignesGrilleDemineur(grille: list) -> int:
+    if type_grille_demineur(grille) == False:
         raise TypeError("« getNbLignesGrilleDemineur : Le paramètre n’est pas une grille")
     return len(grille)
 
-def getNbColonnesGrilleDemineur(grille:list)->int:
-    if type_grille_demineur(grille)==False:
+
+def getNbColonnesGrilleDemineur(grille: list) -> int:
+    if type_grille_demineur(grille) == False:
         raise TypeError("« getNbLignesGrilleDemineur : Le paramètre n’est pas une grille")
     return len(grille[0])
 
-def isCoordonneeCorrecte(grille:list,coord:tuple)-> bool:
-    if type_grille_demineur(grille)==False or type_coordonnee(coord)==False :
+
+def isCoordonneeCorrecte(grille: list, coord: tuple) -> bool:
+    if type_grille_demineur(grille) == False or type_coordonnee(coord) == False:
         raise TypeError("isCoordonneeCorrecte : un des paramètres n'est pas du bon type")
-    res=False
+    res = False
     for i in range(len(grille)):
         for j in range(len(grille[0])):
-            if (i,j)==coord:
-                res=True
+            if (i, j) == coord:
+                res = True
     return res
 
-def getCelluleGrilleDemineur(grille:list,coord:tuple)->dict:
-    if type_grille_demineur(grille)==False or type_coordonnee(coord)==False :
+
+def getCelluleGrilleDemineur(grille: list, coord: tuple) -> dict:
+    if type_grille_demineur(grille) == False or type_coordonnee(coord) == False:
         raise TypeError("isCoordonneeCorrecte : un des paramètres n'est pas du bon type")
-    if isCoordonneeCorrecte(grille,coord)==False:
+    if isCoordonneeCorrecte(grille, coord) == False:
         raise IndexError("getCelluleGrilleDemineur : coordonnée non contenue dans la grille")
     return grille[coord[0]][coord[1]]
 
-def getContenuGrilleDemineur(grille:list,coord:tuple)->int:
+
+def getContenuGrilleDemineur(grille: list, coord: tuple) -> int:
     return grille[coord[0]][coord[1]][const.CONTENU]
 
-def setContenuGrilleDemineur(grille:list,coord:tuple,n:int)-> None:
-    if type_grille_demineur(grille)==False or type_coordonnee(coord)==False or type(n)!=int:
+
+def setContenuGrilleDemineur(grille: list, coord: tuple, n: int) -> None:
+    if type_grille_demineur(grille) == False or type_coordonnee(coord) == False or type(n) != int:
         raise TypeError("SetContenuGrilleDemineur : Le type de l'un ds coordonnées n'est pas correct")
-    if ((0 > n or n> 8) and n != const.ID_MINE) and type(n) == int:
-        raise ValueError("construireCellule : le contenu",n,"n'est pas valide")
-    grille[coord[0]][coord[1]][const.CONTENU]=n
+    if ((0 > n or n > 8) and n != const.ID_MINE) and type(n) == int:
+        raise ValueError("construireCellule : le contenu", n, "n'est pas valide")
+    grille[coord[0]][coord[1]][const.CONTENU] = n
     return None
 
-def isVisibleGrilleDemineur(grille:list,coord:tuple)-> bool:
+
+def isVisibleGrilleDemineur(grille: list, coord: tuple) -> bool:
     return grille[coord[0]][coord[1]][const.VISIBLE]
 
-def setVisibleGrilleDemineur(grille:list,coord:tuple,b:bool)-> None:
-    if type_grille_demineur(grille)==False or type_coordonnee(coord)==False or type(b)!=bool:
+
+def setVisibleGrilleDemineur(grille: list, coord: tuple, b: bool) -> None:
+    if type_grille_demineur(grille) == False or type_coordonnee(coord) == False or type(b) != bool:
         raise TypeError("SetContenuGrilleDemineur : Le type de l'un ds coordonnées n'est pas correct")
-    grille[coord[0]][coord[1]][const.VISIBLE]=b
+    grille[coord[0]][coord[1]][const.VISIBLE] = b
     return None
 
-def contienMineGrilleDemineur(grille:list,coord:tuple)->bool:
-    res=False
-    if grille[coord[0]][coord[1]][const.CONTENU]==cont.ID_MINE:
-        res=True
+
+def contienMineGrilleDemineur(grille: list, coord: tuple) -> bool:
+    res = False
+    if grille[coord[0]][coord[1]][const.CONTENU] == cont.ID_MINE:
+        res = True
+    return res
+
+
+def getCoordonneeVoisinsGrilleDemineur(grille: list, coord: tuple) -> list:
+    res = []
+    if isCoordonneeCorrecte(grille, (coord[0] - 1, coord[1])):
+        res.append((coord[0] - 1, coord[1]))
+    if isCoordonneeCorrecte(grille, (coord[0], coord[1] - 1)):
+        res.append((coord[0], coord[1] - 1))
+    if isCoordonneeCorrecte(grille, (coord[0] + 1, coord[1])):
+        res.append((coord[0] + 1, coord[1]))
+    if isCoordonneeCorrecte(grille, (coord[0], coord[1] + 1)):
+        res.append((coord[0], coord[1] + 1))
+    if isCoordonneeCorrecte(grille, (coord[0] - 1, coord[1] - 1)):
+        res.append((coord[0] - 1, coord[1]))
+    if isCoordonneeCorrecte(grille, (coord[0] + 1, coord[1] + 1)):
+        res.append((coord[0] + 1, coord[1] + 1))
+    if isCoordonneeCorrecte(grille, (coord[0] - 1, coord[1] + 1)):
+        res.append((coord[0] - 1, coord[1] + 1))
+    if isCoordonneeCorrecte(grille, (coord[0] + 1, coord[1] - 1)):
+        res.append((coord[0] + 1, coord[1] - 1))
+    print(res)
     return res
